722f864c09b77ff88d836e40e6be18c5dcfd347e
ryohassay <43590790+ryohassay@users.noreply.github.com>
2024-01-07 18:13:39 +0900

    Embed Google My Map

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -13,9 +13,18 @@ const TopPage = ({ pagesJson }: PageIndexType) => {
             <PageIndex pagesJson={pagesJson} />
             <PageTitle pageTitle={title}/>
             <div className="main">
-                I am an aspiring engineering student who prides myself on my careful work and efficiency. While studying electronics at university, I also have programming experience in C++, Python, and other languages and frameworks. I am interested in both hardware and software. 
-                
-                After graduating from high school in Japan, I have attended university in the US, China and Malaysia, so I am good at teamwork with people from different backgrounds and perspectives.
+                <div className="text">
+                    I am an aspiring engineering student who prides myself on my careful work and efficiency. While studying electronics at university, I also have programming experience in C++, Python, and other languages and frameworks. I am interested in both hardware and software. 
+                    
+                    After graduating from high school in Japan, I have attended university in the US, China and Malaysia, so I am good at teamwork with people from different backgrounds and perspectives.
+                </div>
+
+                <div className="map">
+                    <h3 className="map-title">Schools I Have Attended</h3>
+                    <div className="map-frame">
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1CFD22bWuaEORwg8E8eDhYjD2pgz5xQM&ehbc=2E312F" width="640" height="480"></iframe>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
